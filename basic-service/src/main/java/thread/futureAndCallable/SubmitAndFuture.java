87ae5e130a5321fb36de83869db37aefff357221
yejunjie <88888888>
2017-03-11 09:16:55 +0800

    callback and future

--- basic-service/src/main/java/thread/futureAndCallable/SubmitAndFuture.java
+++ basic-service/src/main/java/thread/futureAndCallable/SubmitAndFuture.java
@@ -6,23 +6,21 @@ import java.util.List;
 import java.util.concurrent.*;
 
 /**
+ * 1：单线程
+ *
+ * 2：invokeall
+ *
+ * 3：submit
+ *
+ * 比较性能
+ *
+ *
  * Created by yejunjie on 2017/3/10.
  */
 public class SubmitAndFuture {
 
-    /**
-     * 批量任务的限时 invokeAll(tasks) 批量提交不限时任务
-     * <p>
-     * invokeAll(tasks, timeout, unit) 批量提交限时任务
-     * <p>
-     * InvokeAll方法处理一个任务的容器（collection），并返回一个Future的容器。两个容器具有相同的结构：
-     * invokeAll将Future添加到返回的容器中，这样可以使用任务容器的迭代器，从而调用者可以将它表现的Callable与Future 关联起来。
-     * 当所有任务都完成时、调用线程被中断时或者超过时限时，限时版本的invokeAll都会返回结果。 超过时限后，任务尚未完成的任务都会被取消。
-     *
-     * @author hadoop
-     */
     // 固定大小的线程池，同时只能接受5个任务
-    static ExecutorService mExecutor = Executors.newFixedThreadPool(10);
+    static ExecutorService mExecutor = Executors.newFixedThreadPool(5);
 
     /**
      * 计算价格的任务
