3db56efc9680a9076b21285c0ecd6ddbbeba3709
yejunjie <88888888>
2017-03-21 13:55:03 +0800

    websocket

---
+++ basic-service/src/main/java/generic/Pair.java
@@ -0,0 +1,30 @@
+package generic;
+
+/**
+ * Created by yejunjie on 2017/3/20.
+ */
+public class Pair<T, U> {
+    private T first;
+    private U second;
+
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public U getSecond() {
+        return second;
+    }
+
+    public void setFirst(T newValue) {
+        first = newValue;
+    }
+
+    public void setSecond(U newValue) {
+        second = newValue;
+    }
+}
