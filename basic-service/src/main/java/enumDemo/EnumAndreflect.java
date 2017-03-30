d00bd54b29959a41c0dd4f5e2057af7c10f79ad0
yejj <yejj@ngarihealth.com>
2017-03-30 18:14:09 +0800

    通过枚举和反射代替switch

--- basic-service/src/main/java/enumDemo/EnumAndreflect.java
+++ basic-service/src/main/java/enumDemo/EnumAndreflect.java
@@ -1,6 +1,7 @@
 package enumDemo;
 
 
+import java.lang.reflect.Method;
 
 /**
  *
@@ -10,5 +11,38 @@ package enumDemo;
  * Created by Administrator on 2017/3/30 0030.
  */
 public class EnumAndreflect {
+
+
+    public static void main(String[] args) {
+        //入参
+        String param = "insert";
+
+        //得到对应的枚举类
+        Actions actions = Enum.valueOf(Actions.class,param);
+
+        //得到方法名
+        String method = actions.getMethod();
+
+        try {
+            Method  m = EnumAndreflect.class.getMethod(method,new Class[]{String.class});
+            m.invoke(new EnumAndreflect(),param);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void m1(String message){
+        System.out.println("m1方法执行" + message);
+    }
+
+    public void m2(String message){
+        System.out.println("m2方法执行"+message);
+    }
+
+    public void m3(String message){
+        System.out.println("m3方法执行"+message);
+    }
+
+
 
 }
